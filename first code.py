0d693760b482de472c72f0ebd8903c807e467a8f
Gowtham9610 <gowtham9610@gmail.com>
2020-05-02 20:31:15 +0530

    this is my first code in python

---
+++ first code.py
@@ -0,0 +1,23 @@
+# this is my first python program
+
+print('hello world')
+# asking for name
+
+print('please enter your name')
+myname=input()
+# print hi with the name
+
+print('Hi '+myname)
+
+print('Enter your age')
+age=input()
+
+print('your age will be '+str(int(age)+1)+ ' in a year')
+
+print('the length of  our name is')
+
+print(len(myname))
+
+
+
+
